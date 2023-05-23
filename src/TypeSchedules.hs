a3f1bd26e973d9a40386429decdd929914a03df0
Will <wveatchw@gmail.com>
2023-05-23 09:41:44 -0400

    working

--- src/TypeSchedules.hs
+++ src/TypeSchedules.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE Arrows #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -5,6 +6,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
@@ -13,6 +15,9 @@ module TypeSchedules where
 
 -- base
 import Data.Kind
+import Control.Concurrent
+import Data.IORef
+import Control.Monad.IO.Class
 
 -- HList
 import Data.HList
@@ -22,6 +27,7 @@ import Data.TimeDomain
 
 -- dunai
 import Data.MonadicStreamFunction hiding (feedback)
+import Data.MonadicStreamFunction.InternalCore hiding (feedback)
 
 newtype Event m cl = Event { unEvent :: TimeInfo cl -> m (Event m cl) }
 
@@ -41,19 +47,29 @@ data ClockedEvent m cl = ClockedEvent { clock :: cl, event :: Event m cl }
 
 -- dont expose: add to an internal module
 data EventNetwork m cls where
-  EN :: Event m cl -> EventNetwork m (HList '[cl])
-  ENCons :: Event m cl -> EventNetwork m (HList cls) -> EventNetwork m (HList (cl ': cls))
+  EN :: Clock m cl => Event m cl -> EventNetwork m (HList '[cl])
+  ENCons
+    :: (Clock m cl0, Clock m cl, Clocks m (HList (cl ': cls)))
+    => Event m cl0
+    -> EventNetwork m (HList (cl ': cls))
+    -> EventNetwork m (HList (cl0 ': cl ': cls))
 
 data Tigris m cls = Tigris { clocks :: cls, en :: EventNetwork m cls }
 
 infixr 6 .:.
-(.:.) :: ClockedEvent m cl -> Tigris m (HList (cls :: [Type])) -> Tigris m (HList (cl ': cls))
-ClockedEvent {..} .:. Tigris {..} = Tigris (HCons clock clocks) (ENCons event en)
+(.:) :: (MonadIO m, Clock m cl0, Clock m cl, Clocks m (HList (cl ': cls)))
+     => ClockedEvent m cl0
+     -> Tigris m (HList (cl ': cls))
+     -> Tigris m (HList (cl0 ': cl ': cls))
+ClockedEvent {..} .: Tigris {..} = Tigris (HCons clock clocks) (ENCons event en)
 
 infixr 6 .:
-(.:) :: ClockedEvent m cl1 -> ClockedEvent m cl2 -> Tigris m (HList (cl1 ': '[cl2]))
-(ClockedEvent clock1 event1) .: (ClockedEvent clock2 event2) =
-  Tigris (HCons clock1 (HCons clock2 HNil)) (ENCons event1 (EN event2))
+(.:.) :: (MonadIO m, Clock m cl1, Clock m cl2)
+      => ClockedEvent m cl1
+      -> ClockedEvent m cl2
+      -> Tigris m (HList (cl1 ': '[cl2]))
+clEv1 .:. (ClockedEvent clock2 event2) =
+  clEv1 .: Tigris (HCons clock2 HNil) (EN event2)
 
 data TimeInfo cl = TimeInfo
   { sinceLast :: Diff (Time cl)
@@ -66,30 +82,127 @@ type RunningClock m time tag = MSF m () (time, tag)
 
 type RunningClockInit m time tag = m (RunningClock m time tag, time)
 
-type RunningClocksInit m info = MSF m () info
+type RunningClocksInit m info = m (MSF m () info)
 
-class Clock m cl where
+class TimeDomain (Time cl) => Clock m cl where
   initClock :: cl -> RunningClockInit m (Time cl) (Tag cl)
   type Time cl
   type Tag  cl
 
 class Clocks m cls where
-  initClocks :: cls -> RunningClocksInit m (ClocksInfo cls)
+  initClocks :: (m () -> IO ()) -> cls -> RunningClocksInit m (ClocksInfo cls)
+  _aux_      :: (m () -> IO ()) -> cls -> m (MVar (ClocksInfo cls), MSF m () ())
   type ClocksInfo cls
 
-instance Clocks m (HList '[]) where
-  initClocks = undefined
-  type ClocksInfo (HList '[]) = ()
+instance (MonadIO m, Clock m cl) => Clocks m (HList (cl ': '[])) where
+  initClocks _ (HCons cl _) = do
+    (runningClock, initTime) <- initClock cl
+    return $ proc _ -> do
+      (absolute, tag) <- runningClock  -< ()
+      lastTime        <- iPre initTime -< absolute
+      returnA                          -< TimeInfo 
+        { sinceLast = absolute `diffTime` lastTime
+        , sinceInit = absolute `diffTime` initTime
+        , ..
+        }
+  _aux_ _ (HCons cl _) = do
+    mvar <- liftIO newEmptyMVar
+    (runningClock, initTime) <- initClock cl
+    return
+      ( mvar
+      , proc _ -> do
+          (absolute, tag) <- runningClock  -< ()
+          lastTime        <- iPre initTime -< absolute
+          arrM (liftIO . putMVar mvar) -< TimeInfo 
+            { sinceLast = absolute `diffTime` lastTime
+            , sinceInit = absolute `diffTime` initTime
+            , ..
+            }
+      )
+
+  type ClocksInfo (HList (cl ': '[])) = TimeInfo cl
   
-instance (Clock m cl, Clocks m (HList cls)) => Clocks m (HList (cl ': cls)) where
-  initClocks = undefined
-  type ClocksInfo (HList (cl ': cls)) = Either (TimeInfo cl) (ClocksInfo (HList cls))
+instance (MonadIO m, Clock m cl1, Clocks m (HList (cl2 ': cls))) => Clocks m (HList (cl1 ': cl2 ': cls)) where
+  initClocks unliftIO (HCons cl cls) = do
+    mvar                     <- liftIO newEmptyMVar
+    (clsMvar, runningClocks) <- _aux_ unliftIO cls
+    -- forks to run first clock
+    _ <- liftIO $ forkIO $ unliftIO $ do
+      (runningClock, initTime) <- initClock cl
+      reactimate $ proc _ -> do
+        (absolute, tag) <- runningClock -< ()
+        lastTime        <- iPre initTime -< absolute
+        arrM (liftIO . putMVar mvar) -< Left $ TimeInfo 
+          { sinceLast = absolute `diffTime` lastTime
+          , sinceInit = absolute `diffTime` initTime
+          , ..
+          }
+    -- forks to run remaining clocks
+    _ <- liftIO $ forkIO $ unliftIO $ reactimate runningClocks
+    -- forks to get infor from remaining clocks and put in main mvar
+    _ <- liftIO $ forkIO $ unliftIO $ reactimate $
+      constM (liftIO $ takeMVar clsMvar)
+      >>> arr Right
+      >>> arrM (liftIO . putMVar mvar)
+    return $ constM $ liftIO $ takeMVar mvar
+
+  _aux_ unliftIO (HCons cl cls) = do
+    mvar                     <- liftIO newEmptyMVar
+    (clsMvar, runningClocks) <- _aux_ unliftIO cls
+    return
+      ( mvar
+      , constM $ do
+          -- forks to run first clock
+          _ <- liftIO $ forkIO $ unliftIO $ do
+            (runningClock, initTime) <- initClock cl
+            reactimate $ proc _ -> do
+              (absolute, tag) <- runningClock -< ()
+              lastTime        <- iPre initTime -< absolute
+              arrM (liftIO . putMVar mvar) -< Left $ TimeInfo 
+                { sinceLast = absolute `diffTime` lastTime
+                , sinceInit = absolute `diffTime` initTime
+                , ..
+                }
+          -- forks to run remaining clocks
+          _ <- liftIO $ forkIO $ unliftIO $ reactimate runningClocks
+          -- forks to get infor from remaining clocks and put in main mvar
+          _ <- liftIO $ forkIO $ unliftIO $ reactimate $
+            constM (liftIO $ takeMVar clsMvar)
+            >>> arr Right
+            >>> arrM (liftIO . putMVar mvar)
+          return ()
+      )
+
+  type ClocksInfo (HList (cl1 ': cl2 ': cls))
+    = Either (TimeInfo cl1) (ClocksInfo (HList (cl2 ': cls)))
+
 
 eraseClocks
-  :: (Monad m, Clocks m cls)
-  => Tigris m cls
-  -> m ()
-eraseClocks Tigris {..} = do
-  undefined
+  :: (MonadIO m, Clocks m (HList (cl ': cls)))
+  => Tigris m (HList (cl ': cls))
+  -> (m () -> IO ())
+  -> m (MSF m () ())
+eraseClocks Tigris {..} unliftIO = do
+  runningClocks <- initClocks unliftIO clocks
+  return $ proc _ -> do
+    clsInfo <- runningClocks -< ()
+    eraseClocksEN en -< clsInfo
 
---eraseClocksEN :: (Monad m, Clocks cls) => EventNetwork m cls -> MSF m (ClocksInfo
+toMSF :: Monad m => Event m cl -> MSF m (TimeInfo cl) ()
+toMSF event = MSF $ \clInfo -> do
+  event' <- (unEvent event) clInfo
+  return ((), toMSF event')
+
+eraseClocksEN
+  :: (Monad m, Clocks m (HList (cl ': cls)))
+  => EventNetwork m (HList (cl ': cls))
+  -> MSF m (ClocksInfo (HList (cl ': cls))) ()
+eraseClocksEN (EN event) = proc clInfo -> do
+  toMSF event -< clInfo
+eraseClocksEN (ENCons event eventNetwork) = proc clsInfo -> do
+  case clsInfo of
+    Left clInfo    -> do
+      toMSF event -< clInfo
+    Right clsInfo' -> do
+      eraseClocksEN eventNetwork -< clsInfo'
+
